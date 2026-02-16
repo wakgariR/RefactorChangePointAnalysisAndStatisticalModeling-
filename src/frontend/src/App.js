a34d4559d84ebf87567e0093649945a194c693a9
wakgariR <wakgarirobel@gmail.com>
2026-02-16 10:56:59 +0300

    add material ui and toolbar on app

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -5,6 +5,18 @@ import {
   fetchChangePoints,
   fetchMetrics,
 } from "./api";
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+
+import Switch from "@mui/material/Switch";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormGroup from "@mui/material/FormGroup";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
 import PriceChart from "./components/PriceChart";
 import EventTimeline from "./components/EventTimeline";
 import Filters from "./components/Filters";
@@ -23,15 +35,43 @@ function App() {
     fetchMetrics().then(setMetrics);
   }, []);
 
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
-    <div className="container">
-      <h1>Brent Oil Market Dashboard</h1>
-      <MetricsCards metrics={metrics} />
-      <br />
-      <Filters onFilter={setPrices} />
-      <PriceChart prices={prices} events={events} changePoints={changePoints} />
-      <EventTimeline events={events} />
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          ></IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Brent Oil Market Dashboard
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <div className="container">
+        <MetricsCards metrics={metrics} />
+        <br />
+        <Filters onFilter={setPrices} />
+        <PriceChart
+          prices={prices}
+          events={events}
+          changePoints={changePoints}
+        />
+        <EventTimeline events={events} />
+      </div>
+    </Box>
   );
 }
 
